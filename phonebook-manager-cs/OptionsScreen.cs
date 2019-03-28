d64ad5b2c918af6581f463fc380ec52254cfccde
Kevin A Mathew <kevinam99@gmail.com>
2019-03-28 23:47:45 +0530

    Created new form 'OptionsScreen'

---
+++ phonebook-manager-cs/OptionsScreen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace phonebook_manager_cs
+{
+    public partial class OptionsScreen : Form
+    {
+        public OptionsScreen()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void OptionsScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OptionsScreen_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            
+        }
+    }
+}
