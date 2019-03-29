2305be1a1a6d7a4d0c866609bd118a8b5374cbe8
Kevin A Mathew <kevinam99@gmail.com>
2019-03-29 12:25:21 +0530

    Developed full frontend.

--- phonebook-manager-cs/Update_Contact.Designer.cs
+++ phonebook-manager-cs/Update_Contact.Designer.cs
@@ -28,12 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SourceCode_linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.Exit_button1 = new System.Windows.Forms.Button();
+            this.Save_button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.CreateName_label1 = new System.Windows.Forms.Label();
+            this.CreateContactHeader_label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SourceCode_linkLabel1
+            // 
+            this.SourceCode_linkLabel1.AutoSize = true;
+            this.SourceCode_linkLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.SourceCode_linkLabel1.Font = new System.Drawing.Font("Consolas", 9.75F);
+            this.SourceCode_linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.AlwaysUnderline;
+            this.SourceCode_linkLabel1.Location = new System.Drawing.Point(440, 287);
+            this.SourceCode_linkLabel1.Name = "SourceCode_linkLabel1";
+            this.SourceCode_linkLabel1.Size = new System.Drawing.Size(84, 15);
+            this.SourceCode_linkLabel1.TabIndex = 33;
+            this.SourceCode_linkLabel1.TabStop = true;
+            this.SourceCode_linkLabel1.Text = "Source code";
+            // 
+            // Exit_button1
+            // 
+            this.Exit_button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Exit_button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.Exit_button1.Location = new System.Drawing.Point(331, 242);
+            this.Exit_button1.Name = "Exit_button1";
+            this.Exit_button1.Size = new System.Drawing.Size(193, 33);
+            this.Exit_button1.TabIndex = 32;
+            this.Exit_button1.Text = "Exit";
+            this.Exit_button1.UseVisualStyleBackColor = true;
+            // 
+            // Save_button1
+            // 
+            this.Save_button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.Save_button1.Location = new System.Drawing.Point(54, 242);
+            this.Save_button1.Name = "Save_button1";
+            this.Save_button1.Size = new System.Drawing.Size(193, 33);
+            this.Save_button1.TabIndex = 31;
+            this.Save_button1.Text = "Save";
+            this.Save_button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Century Gothic", 11.25F);
+            this.textBox3.Location = new System.Drawing.Point(241, 180);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(283, 26);
+            this.textBox3.TabIndex = 30;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Century Gothic", 11.25F);
+            this.textBox2.Location = new System.Drawing.Point(241, 124);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(283, 26);
+            this.textBox2.TabIndex = 29;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Constantia", 15.75F);
+            this.label2.Location = new System.Drawing.Point(49, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 26);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Email";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Constantia", 15.75F);
+            this.label1.Location = new System.Drawing.Point(49, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 26);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "Contact number";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 11.25F);
+            this.textBox1.Location = new System.Drawing.Point(241, 61);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(283, 26);
+            this.textBox1.TabIndex = 26;
+            // 
+            // CreateName_label1
+            // 
+            this.CreateName_label1.AutoSize = true;
+            this.CreateName_label1.BackColor = System.Drawing.Color.Transparent;
+            this.CreateName_label1.Font = new System.Drawing.Font("Constantia", 15.75F);
+            this.CreateName_label1.Location = new System.Drawing.Point(49, 61);
+            this.CreateName_label1.Name = "CreateName_label1";
+            this.CreateName_label1.Size = new System.Drawing.Size(66, 26);
+            this.CreateName_label1.TabIndex = 25;
+            this.CreateName_label1.Text = "Name";
+            // 
+            // CreateContactHeader_label1
+            // 
+            this.CreateContactHeader_label1.AutoSize = true;
+            this.CreateContactHeader_label1.BackColor = System.Drawing.Color.Transparent;
+            this.CreateContactHeader_label1.Font = new System.Drawing.Font("Palatino Linotype", 20.25F);
+            this.CreateContactHeader_label1.Location = new System.Drawing.Point(165, 9);
+            this.CreateContactHeader_label1.Name = "CreateContactHeader_label1";
+            this.CreateContactHeader_label1.Size = new System.Drawing.Size(256, 36);
+            this.CreateContactHeader_label1.TabIndex = 24;
+            this.CreateContactHeader_label1.Text = "Create a new contact";
+            // 
+            // Update_Contact
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(566, 310);
+            this.Controls.Add(this.SourceCode_linkLabel1);
+            this.Controls.Add(this.Exit_button1);
+            this.Controls.Add(this.Save_button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.CreateName_label1);
+            this.Controls.Add(this.CreateContactHeader_label1);
+            this.Name = "Update_Contact";
             this.Text = "Update_Contact";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.LinkLabel SourceCode_linkLabel1;
+        private System.Windows.Forms.Button Exit_button1;
+        private System.Windows.Forms.Button Save_button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label CreateName_label1;
+        private System.Windows.Forms.Label CreateContactHeader_label1;
     }
 }
