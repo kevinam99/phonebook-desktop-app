d45d50eedf2a0e1f1f9857fb0d1073739ad0e62d
Kevin A Mathew <kevinam99@gmail.com>
2019-03-30 12:51:42 +0530

    Developed full frontend.

--- phonebook-manager-cs/Delete_Contact.Designer.cs
+++ phonebook-manager-cs/Delete_Contact.Designer.cs
@@ -28,10 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Delete_Contact));
+            this.SuspendLayout();
+            // 
+            // Delete_Contact
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Delete_Contact";
+            this.ClientSize = new System.Drawing.Size(572, 304);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Delete_Contact";
+            this.Text = "Phonebook Manager";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
