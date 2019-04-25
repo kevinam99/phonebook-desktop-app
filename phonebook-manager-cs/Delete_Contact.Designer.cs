5ba42b8417e09f1ab88a13be7320d2b672295479
Kevin A Mathew <kevinam99@gmail.com>
2019-04-25 13:15:43 +0530

    added delete DB logic

--- phonebook-manager-cs/Delete_Contact.Designer.cs
+++ phonebook-manager-cs/Delete_Contact.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Delete_Contact));
             this.DeleteContactHeader_label1 = new System.Windows.Forms.Label();
             this.SourceCode_linkLabel1 = new System.Windows.Forms.LinkLabel();
@@ -35,6 +36,19 @@
             this.TextboxUserID = new System.Windows.Forms.Label();
             this.Delete_button2 = new System.Windows.Forms.Button();
             this.Exit_button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataSet1 = new phonebook_manager_cs.DataSet1();
+            this.dataSet1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.cONTACTSBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.cONTACTSTableAdapter = new phonebook_manager_cs.DataSet1TableAdapters.CONTACTSTableAdapter();
+            this.uSERIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nAMEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cONTACTNUMBERDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.eMAILDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cONTACTSBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // DeleteContactHeader_label1
@@ -92,6 +106,7 @@
             this.Delete_button2.TabIndex = 22;
             this.Delete_button2.Text = "Delete contact";
             this.Delete_button2.UseVisualStyleBackColor = true;
+            this.Delete_button2.Click += new System.EventHandler(this.Delete_button2_Click);
             // 
             // Exit_button3
             // 
@@ -104,6 +119,64 @@
             this.Exit_button3.UseVisualStyleBackColor = true;
             this.Exit_button3.Click += new System.EventHandler(this.Exit_button3_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.uSERIDDataGridViewTextBoxColumn,
+            this.nAMEDataGridViewTextBoxColumn,
+            this.cONTACTNUMBERDataGridViewTextBoxColumn,
+            this.eMAILDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.cONTACTSBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(61, 42);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(441, 150);
+            this.dataGridView1.TabIndex = 37;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dataSet1BindingSource
+            // 
+            this.dataSet1BindingSource.DataSource = this.dataSet1;
+            this.dataSet1BindingSource.Position = 0;
+            // 
+            // cONTACTSBindingSource
+            // 
+            this.cONTACTSBindingSource.DataMember = "CONTACTS";
+            this.cONTACTSBindingSource.DataSource = this.dataSet1;
+            // 
+            // cONTACTSTableAdapter
+            // 
+            this.cONTACTSTableAdapter.ClearBeforeFill = true;
+            // 
+            // uSERIDDataGridViewTextBoxColumn
+            // 
+            this.uSERIDDataGridViewTextBoxColumn.DataPropertyName = "USERID";
+            this.uSERIDDataGridViewTextBoxColumn.HeaderText = "USERID";
+            this.uSERIDDataGridViewTextBoxColumn.Name = "uSERIDDataGridViewTextBoxColumn";
+            // 
+            // nAMEDataGridViewTextBoxColumn
+            // 
+            this.nAMEDataGridViewTextBoxColumn.DataPropertyName = "NAME";
+            this.nAMEDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nAMEDataGridViewTextBoxColumn.Name = "nAMEDataGridViewTextBoxColumn";
+            // 
+            // cONTACTNUMBERDataGridViewTextBoxColumn
+            // 
+            this.cONTACTNUMBERDataGridViewTextBoxColumn.DataPropertyName = "CONTACT_NUMBER";
+            this.cONTACTNUMBERDataGridViewTextBoxColumn.HeaderText = "Contact Number";
+            this.cONTACTNUMBERDataGridViewTextBoxColumn.Name = "cONTACTNUMBERDataGridViewTextBoxColumn";
+            // 
+            // eMAILDataGridViewTextBoxColumn
+            // 
+            this.eMAILDataGridViewTextBoxColumn.DataPropertyName = "EMAIL";
+            this.eMAILDataGridViewTextBoxColumn.HeaderText = "E-mail";
+            this.eMAILDataGridViewTextBoxColumn.Name = "eMAILDataGridViewTextBoxColumn";
+            // 
             // Delete_Contact
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -112,6 +185,7 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(565, 296);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.Exit_button3);
             this.Controls.Add(this.Delete_button2);
             this.Controls.Add(this.SourceCode_linkLabel1);
@@ -125,6 +199,11 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Phonebook Manager";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Delete_Contact_FormClosing);
+            this.Load += new System.EventHandler(this.Delete_Contact_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cONTACTSBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -138,5 +217,14 @@
         private System.Windows.Forms.Label TextboxUserID;
         private System.Windows.Forms.Button Delete_button2;
         private System.Windows.Forms.Button Exit_button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource dataSet1BindingSource;
+        private DataSet1 dataSet1;
+        private System.Windows.Forms.BindingSource cONTACTSBindingSource;
+        private DataSet1TableAdapters.CONTACTSTableAdapter cONTACTSTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn uSERIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nAMEDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cONTACTNUMBERDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn eMAILDataGridViewTextBoxColumn;
     }
 }
--- phonebook-manager-cs/Delete_Contact.cs
+++ phonebook-manager-cs/Delete_Contact.cs
@@ -14,9 +14,14 @@ namespace phonebook_manager_cs
 {
     public partial class Delete_Contact : Form
     {
+        public string ConnectionString;
+
+       
+
         public Delete_Contact()
         {
             InitializeComponent();
+            ConnectionString = Create_Contact.ConnectionString;
         }
 
         private void Exit_button3_Click(object sender, EventArgs e)
@@ -54,6 +59,42 @@ namespace phonebook_manager_cs
             else if (dialog == DialogResult.Cancel)
             {
                 e.Cancel = true;
+            }
+        }
+
+        private void Delete_Contact_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dataSet1.CONTACTS' table. You can move, or remove it, as needed.
+            this.cONTACTSTableAdapter.Fill(this.dataSet1.CONTACTS);
+
+        }
+
+        private void Delete_button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!(textBox1.Text == ""))
+                {
+                    string id = textBox1.Text;
+                    
+                    string sql = "Delete FROM Contacts WHERE UserId = '" + id + "' ";
+                    //Oracle connection
+                    OracleConnection con = new OracleConnection(ConnectionString);
+                    con.Open();
+                    OracleCommand cmd = new OracleCommand(sql, con);
+                    cmd.CommandType = CommandType.Text;
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Successfully deleted contact '");
+                }
+                else
+                {
+                    MessageBox.Show("Please add valid inputs");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw ex;
             }
         }
     }
